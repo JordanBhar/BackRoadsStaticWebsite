e543eaebd4d9cba03f90f451346b10dc570b2cd8
JordanBhar <jordan.bhar@hotmail.com>
2022-06-14 18:22:08 -0400

    Commit 16: Re-did the whole app image showcase courasel, this made it so the images are properly sized and centered with mobile views or regular screen views

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
 window.onscroll = function(){scrollTransparentNavbar()} 
 
 function scrollTransparentNavbar (){
-
-    console.log(document.body.scrollTop)
+    
     var navbar = document.getElementById("navTransparent")
-    console.log(navbar)
-
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    
+    
+    if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100){
         navbar.style.backgroundColor = 'rgb(72, 0, 155)';
         navbar.style.zIndex = "2"
         navbar.style.transition = "1s"
         
     }else{
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
         navbar.style.zIndex = "1"
     }
 
