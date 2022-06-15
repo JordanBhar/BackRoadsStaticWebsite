4468f35170310b9fa6edc54002f8784eb2936fec
JordanBhar <jordan.bhar@hotmail.com>
2022-06-14 22:50:16 -0400

    Commit 17: Last Feature Commit, Anything beyond this point is last touch ups or code elimination

--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-window.onscroll = function(){scrollTransparentNavbar()} 
+    
 
-function scrollTransparentNavbar (){
-    
-    var navbar = document.getElementById("navTransparent")
-    
-    
-    if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100){
-        navbar.style.backgroundColor = 'rgb(72, 0, 155)';
-        navbar.style.zIndex = "2"
-        navbar.style.transition = "1s"
-        
-    }else{
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-        navbar.style.zIndex = "1"
+    document.body.addEventListener("touchstart", playVideo)
+
+    function playVideo() {
+
+        const banner = document.getElementById("animatedBanner")
+
+        if (banner.playing) {
+
+        } else {
+            banner.play()
+        }
     }
 
-}
+
+    window.onscroll = function () { scrollTransparentNavbar() }
+
+    function scrollTransparentNavbar() {
+
+        var navbarCollapsables = document.getElementById("navbarSupportedContent")
+        var navbar = document.getElementById("navTransparent")
+
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            setTimeout(function () {
+                navbarCollapsables.style.backgroundColor = '#48009b';
+                navbar.style.backgroundColor = '#48009b';
+            }, 200);
+
+            navbar.style.zIndex = "2"
+        } else {
+
+            setTimeout(function () {
+                navbarCollapsables.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+                navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+            }, 200);
+
+
+            navbar.style.zIndex = "1"
+        }
+
+    }
 
 
 })
